188abacff7e7faf622032423b1afbcff68eaf681
hortonjess41-design <hortonjess41@gmail.com>
2026-05-12 15:58:42 -0700

    Update odds.js

--- api/odds.js
+++ api/odds.js
@@ -1,9 +1,18 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const { path, ...params } = req.query;
   const query = new URLSearchParams(params).toString();
   const url = `https://api.the-odds-api.com/v4/${path}?${query}`;
+
   const response = await fetch(url);
   const data = await response.json();
-  res.setHeader("Access-Control-Allow-Origin", "*");
+
   res.status(response.status).json(data);
 }
